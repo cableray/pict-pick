9569c0d0279745947a55aa2d2c05ebf8f2633bf8
cableray <cableraywire@gmail.com>
2013-02-09 20:24:54 -0700

    Initial rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PictPick::Application.config.secret_key_base = '578c4529c394d8de200f420a05ea6725f294225e923b0c2a866079099b025ac9f812033917b8bb14efbe04713d3a6faf7c08376fe73d5bb67e425176187639b7'
