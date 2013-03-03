7392eaff2cc72a0ec10a3dc910d8ef59d50ed528
cableray <cableraywire@gmail.com>
2013-03-02 18:01:02 -0700

    change generated images controller spec to use factory for valid_attributes

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -24,7 +24,7 @@ describe ImagesController do
   # Image. As you add validations to Image, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    { "title" => "MyString" }
+    attributes_for(:image)
   end
 
   # This should return the minimal set of values that should be in the session
